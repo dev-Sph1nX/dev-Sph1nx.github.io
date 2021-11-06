79dfbdc0ce8575860f0b4c62f4fa32053e83d4c5
dev-Sph1nX <lucasleperlier1010@gmail.com>
2021-11-06 18:52:32 +0100

    loading anim finished

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
+function create(tag, parent, text = null, classs = null) {
+    let element = document.createElement(tag)
+    parent.appendChild(element)
+    if (text)
+        element.appendChild(document.createTextNode(text))
+    if (classs)
+        element.classList.add(classs)
+    return element
+}
+
+function count(p) {
+    let count = 0
+    let tag = true
+    if (tag) {
+        setInterval(function() {
+            if (count <= 100)
+                p.innerHTML = count
+            else
+                tag = false
+            count++
+        }, 1)
+    }
+
+}
+
+function createcircle(parent) {
+    let loading = create("div", parent)
+    loading.setAttribute('id', 'loading')
+    let left = create("div", loading, "", "hold")
+    left.classList.add("left")
+    let right = create("div", loading, "", "hold")
+    right.classList.add("right")
+    create("div", left, "", "fill")
+    create("div", right, "", "fill")
+}
+
 document.addEventListener("DOMContentLoaded", function() {
 
+    let allcontent = document.querySelector(".all-content")
+    let ladiv = document.querySelector(".la")
+    let loading = document.querySelector("#loading")
+    let lasvg = document.querySelector(".la svg")
+    let lapercent = document.querySelector(".la-text-percentage")
+    let latext = document.querySelector(".la-text")
+
+    setTimeout(function() {
+        loading.remove()
+        let circle = create("div", ladiv, "", 'circle')
+        count(lapercent)
+        setTimeout(function() {
+            circle.remove()
+            createcircle(ladiv)
+            latext.classList.add("la-text-rotate-second")
+            setTimeout(function() {
+                ladiv.remove()
+                create("div", allcontent, "Bienvenue sur le site", "home")
+            }, 1500)
+        }, 2000)
+    }, 1000)
 })
