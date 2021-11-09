4eaf0a27fffd3a2712877f63b2e965becb9e29ea
dev-Sph1nX <lucasleperlier1010@gmail.com>
2021-11-09 15:53:23 +0100

    beginning of the scroll page
    So nice, i'm fast as fck boyy

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function create(tag, parent, text = null, classs = null) {
+function create(tag, parent, text = null, classs = null) { //creating html element tool 
     let element = document.createElement(tag)
     parent.appendChild(element)
     if (text)
@@ -8,7 +8,7 @@ function create(tag, parent, text = null, classs = null) {
     return element
 }
 
-function count(p) {
+function count(p) { // 0 to 100 graphicaly
     let count = 0
     let tag = true
     if (tag) {
@@ -23,7 +23,7 @@ function count(p) {
 
 }
 
-function createcircle(parent) {
+function createcircle(parent) { // create the circle use to the animation
     let loading = create("div", parent)
     loading.setAttribute('id', 'loading')
     let left = create("div", loading, "", "hold")
@@ -33,7 +33,32 @@ function createcircle(parent) {
     create("div", left, "", "fill")
     create("div", right, "", "fill")
 }
-
+function checkandchangepage(){
+    switch(sessionStorage.getItem('idpage')){
+        case "1":
+            console.log("Page : Home // idpage :" + sessionStorage.getItem('idpage'))
+            $(function(){
+                $("#maincontent").load("home-content.html"); 
+            })
+            break
+        case "2":
+            console.log("Page : CV // idpage :" + sessionStorage.getItem('idpage'))
+            $(function(){
+                $("#maincontent").load("CV-content.html"); 
+            });
+            break
+        case "3":
+            console.log("Page : Contact // idpage :" + sessionStorage.getItem('idpage'))
+            $(function(){
+                $("#maincontent").load("contact-content.html"); 
+            });
+            break
+        default:
+            console.log("Au sekours // idpage :" + sessionStorage.getItem('idpage'))
+            sessionStorage.setItem('idpage', 1)
+            break
+    }
+}
 document.addEventListener("DOMContentLoaded", function() {
 
     let allcontent = document.querySelector(".all-content")
@@ -42,27 +67,48 @@ document.addEventListener("DOMContentLoaded", function() {
     let lasvg = document.querySelector(".la svg")
     let lapercent = document.querySelector(".la-text-percentage")
     let latext = document.querySelector(".la-text")
-
     let body = document.querySelector(".body")
+    let indicpageDiv = document.querySelector(".b-footer-indic-page")
 
-    setTimeout(function() {
-        loading.remove()
+
+    sessionStorage.setItem('roll', 0);
+
+    let pages = ["home", "CV", "Contact"]
+    sessionStorage.setItem('idpage', 1);
+    
+    setTimeout(function() { // Beginning animation
+        loading.remove() // End 1st circle
         let circle = create("div", ladiv, "", 'circle')
         count(lapercent)
-        setTimeout(function() {
+        setTimeout(function() { // End border animation
             circle.remove()
             createcircle(ladiv)
             latext.classList.add("la-text-rotate-second")
             ladiv.classList.add("fondu-out")
-            setTimeout(function() {
+            setTimeout(function() { //End 2nd circle
                 ladiv.remove()
                 body.classList.remove("hidden")
-                body.classList.add("fondu-in")
+                body.classList.add("fondu-in") // let the body appear
             }, 1200)
         }, 2000)
     }, 1000)
+
+    window.addEventListener('wheel', function(event){ // Check mouse wheel
+        if(event.deltaY > 0)
+            sessionStorage.setItem('roll', parseInt(sessionStorage.getItem('roll'))+1) //Increment
+        else if(event.deltaY < 0)
+            sessionStorage.setItem('roll', parseInt(sessionStorage.getItem('roll'))-1) // Decrement
+    
+        if(parseInt(sessionStorage.getItem('roll')) > 2){
+            sessionStorage.setItem('idpage', parseInt(sessionStorage.getItem('idpage'))+1)
+            sessionStorage.setItem('roll', 0);
+        }
+        else if(parseInt(sessionStorage.getItem('roll')) < -2){
+            sessionStorage.setItem('idpage', parseInt(sessionStorage.getItem('idpage'))-1)
+            sessionStorage.setItem('roll', 0);
+        }
+
+        checkandchangepage();
+    });
 })
 
-window.addEventListener('wheel', function(event){
-    console.log(event);
-});
